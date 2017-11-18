334037a102866cb944db8c42eeca2bbeaa043d4d
Declan <declanatkins@gmail.com>
2017-11-18 11:57:53 +0000

    predficted outliers

--- loadData.py
+++ loadData.py
@@ -29,7 +29,27 @@ def fillNullValues(data):
                     data.iloc[i,data.columns.get_loc(col)] = newVal
     
     return data
+
+def removeOutliers(data):
+    for i,row in data.iterrows():
+        for col in data:
+            if (col == 'TAVG' or col == 'TMIN' or col == 'TMAX'):
+                surroundVals = []
+                for ind in range(i-3,i+3):
+                    if ind != i:
+                        try:
+                            surroundVals.append(data.iloc[ind][col])
+                        except Exception as e:
+                            pass
+
+                s = sum(surroundVals)
+                avg = s/len(surroundVals)
+                if(avg > 20 + row[col] or row[col] > 20 + avg):
+                    data.iloc[i,data.columns.get_loc(col)] = avg
+    
+    return data
     
 
 data = fillNullValues(readData())
-data.to_csv('DataHack 2017 Data/Null-filled-concat.csv')
+data = removeOutliers(data)
+data.to_csv('DataHack 2017 Data/Null-filled-rem-outliers.csv')
