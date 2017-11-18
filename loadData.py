c6c321fc31c81b671cb211f30c1de6283cef87e2
Declan <declanatkins@gmail.com>
2017-11-18 10:06:30 +0000

    Neg removed from bikes

--- loadData.py
+++ loadData.py
@@ -6,9 +6,29 @@ def readData():
     Creates a pandas data frame from the 
     csv files
     '''
-    bikesData = pd.read_csv('Datahack 2017 Data/bikes.csv')
+    bikesData = pd.read_csv('Datahack 2017 Data/bikes.csv', names=['Date', 'Count'])
     holidaysData = pd.read_csv('Datahack 2017 Data/holidays.csv')
     weatherData = pd.read_csv('Datahack 2017 Data/weather.csv')
+    return bikesData,holidaysData,weatherData
+
+
+def countMatches(bikesData,weatherData):
+    weatherDates = list(weatherData.Date)
+    bikesDates = list(bikesData.Date)
+    count = 0
+    for date in weatherDates:
+        if date in bikesDates:
+            count += 1
+    print(weatherData)
+    print(count)
+
+
+bd,hd,wd = readData()
+dateW = list(wd.Date)
+dateB = list(bd.Date)
+for d in dateB:
+    if d not in dateW:
+        print(d)
 
 
 
